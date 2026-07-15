6ca8339ed52a1425355b8cda7f9c917a7d9157ec
Italo Araujo Chaves <italoaraujosantos@gmail.com>
2026-07-15 00:15:08 -0300

    Exercicio 1 da atividade 4

--- src/classRoom4/Objective1.java
+++ src/classRoom4/Objective1.java
@@ -5,27 +5,28 @@ import java.util.Scanner;
 public class Objective1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int teste = 5;
+       int tentativas = 5;
         String senha;
         do {
-            senha = scanner.toString();
-            scanner.nextLine();
-            abrirCofre(senha, 5);
-            teste--;
-        } while(teste != 0);
+            senha = scanner.nextLine();
+            abrirCofre(senha, tentativas);
+            tentativas--;
+        } while(tentativas > 0);
+        scanner.close();
     }
 
     public static void abrirCofre(String senha, int tentativas) {
-        String[] valor = new String[0];
+        String[] valor = new String[tentativas];
         int indice = 0;
         if(senha.equals("9999")) {
             System.out.println("Cofre Aberto!");
+            break;
         } else if(tentativas > 0) {
             tentativas--;
             indice++;
             valor[indice] = senha;
             System.out.println("Código Errado. Restam "+tentativas+"...");
-        }else if(tentativas == -1) {
+        }else if(tentativas == 0) {
             indice++;
             valor[indice] = senha;
             System.out.print(valor+", ");
