742de1763acacb34d25053cee3bc4d9822ebe86f
Italo Araujo Chaves <italoaraujosantos@gmail.com>
2026-07-16 21:45:25 -0300

    Altera aula 5

--- src/activies/FifthClass.java
+++ src/activies/FifthClass.java
@@ -1,37 +1,53 @@
 package activies;
 
+import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class FifthClass {
     public static void main(String[] args) {
-        // Create an array of integers
-        int[] numbers = {1, 2, 3, 4, 5};
+        Scanner entrada = new Scanner(System.in);
 
-        // Print the array of integers
-        System.out.print("Array elements: ");
-        for (int number : numbers) {
-            System.out.print(number + " ");
+        //Declarado vetores
+        int[] numeros = new int[3];
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+
+        String[] nomes = new String[]{
+                "João", "Ithalo", "Daniel"
+        };
+
+        //foreach
+        for (int numero : numeros) {
+            System.out.println(numero);
         }
 
-        // Convert the array to an ArrayList
-        ArrayList<Integer> numberList = new ArrayList<>();
+        //Declarando arraylist
+        ArrayList<String> listaDeNomes = new ArrayList<>();
 
-        // add elements from the array to the ArrayList
-        for (int number : numbers) {
-            numberList.add(number);
+        int opcao = 0;
+        while (opcao == 0) {
+            int opcaoLista;
+            System.out.println("Escolha uma opção:");
+            System.out.println("Adicionar = 1\nApagar = 2");
+            opcaoLista = entrada.nextInt();
+            entrada.nextLine();
+            if (opcaoLista == 1) {
+                System.out.println("Digite um nome ADICIONAR: ");
+                //Adiciona algum valor na primeira posição
+                //Se ja tiver um valor na primeira, ele insere na segunda posição
+                listaDeNomes.add(entrada.nextLine());
+            } else {
+                System.out.println("Digite um nome APAGAR: ");
+                //Retira o nome que ta na primeira posição
+                listaDeNomes.remove(10);
+            }
+
+            System.out.println("Continuar - 0");
+            System.out.println("Sair - 1");
+
+            opcao = entrada.nextInt();
+            entrada.nextLine();
         }
-
-        // Create an array of Strings
-        String[] names = {"Italo","Gilmara", "Alessandro", "Euller"};
-
-        // Convert the array to an ArrayList
-        ArrayList<String> nameList = new ArrayList<>();
-
-        // add elements the array to the ArrayList
-        Collections.addAll(nameList, names);
-
     }
-
-
 }
