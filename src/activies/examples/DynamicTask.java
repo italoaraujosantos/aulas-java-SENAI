da93cd27211e45457eef75637fb3d3b13392dcea
Italo Araujo Chaves <italoaraujosantos@gmail.com>
2026-07-16 23:34:12 -0300

    Adiciona exemplos

---
+++ src/activies/examples/DynamicTask.java
@@ -0,0 +1,28 @@
+package activies.examples;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DynamicTask {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        ArrayList<String> tarefas = new ArrayList<>();
+
+        while (true){
+            System.out.print("Digite uma tarefa: ");
+            tarefas.add(entrada.nextLine());
+
+            System.out.println("Deseja continuar?");
+            String opcao = entrada.nextLine();
+
+            if (opcao.equals("fim")){
+                break;
+            }
+        }
+        System.out.println("Total de tarefas registradas: "+tarefas.size());
+
+        for (int i = 0; i < tarefas.size(); i++) {
+            System.out.println((i+1)+". "+tarefas.get(i));
+        }
+    }
+}
